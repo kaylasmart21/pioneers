f6b6c0f24d003ca54aefcf527fac885580838fb9
kaylasmart21 <kaylasmart21@gmail.com>
2017-06-21 15:17:20 +0000

    Update code.py
    uploading our pioneers challenge 2 code

--- outdoors/code.py
+++ outdoors/code.py
@@ -1,1 +1,21 @@
+from picamera import PiCamera
+from blinkt import set_pixel, set_brightness, show, clear, set_all
+from time import sleep, gmtime, strftime
 
+set_brightness(0.5)
+camera=PiCamera()
+
+while True:
+    for i in range(8):
+              clear()
+              set_pixel(i,225, 0, 255)
+              show()
+              sleep(1)
+    clear()
+    set_all(0, 255, 0)
+    show()
+    sleep(2)
+    clear()
+    output = strftime("/home/pi/Documents/trampoline/image-%d-%m %H:%M:%S.jpg", gmtime())
+    camera.capture(output)
+    sleep(2)
